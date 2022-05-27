27be0da0e3bcdbde87347734ac6457f74e682a88
Neville <>
2022-05-27 12:31:52 +0800

    update

--- web.js
+++ web.js
@@ -10,6 +10,7 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   console.log(req.body);
+  console.log('hello');
   const moves = ['F', 'T', 'L', 'R'];
   res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
